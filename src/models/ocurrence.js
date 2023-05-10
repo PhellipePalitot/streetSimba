e3c5591de903f602c6a74684f7ef304292f6531b
Victoria Monteiro <vmp309@gmail.com>
2023-05-10 15:42:12 -0300

    Creating Ocurrence class and creating files for controller and service folders

---
+++ src/models/ocurrence.js
@@ -0,0 +1,10 @@
+class Ocurrence {
+  constructor(local, time, date, description){
+    this.local = local
+    this.time = time
+    this.date = date
+    this.description = description
+  }
+}
+
+export default Ocurrence
