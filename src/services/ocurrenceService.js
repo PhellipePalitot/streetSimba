ac7f0493a59b33fe92942068e1cd9c026361b3f0
Victoria Monteiro <vmp309@gmail.com>
2023-05-11 12:03:42 -0300

    Criacao da classe OcurrenceService no padrao Singleton

---
+++ src/services/ocurrenceService.js
@@ -0,0 +1,33 @@
+import Ocurrence from "../models/ocurrence";
+class OcurrenceService {
+
+  constructor(){
+    //Este constructor garante que a classe produza apenas uma instancia 
+    //(Considere o array como uma database que só pode ser instanciada uma vez)
+    if(OcurrenceService.Ocurrencelist){
+      return OcurrenceService.Ocurrencelist;
+    }
+
+    this.Ocurrencelist = new Map();
+    OcurrenceService.Ocurrencelist = this;
+    return this;
+  }
+  
+  getOcurrence(ocurrence){
+    return this.Ocurrencelist[ocurrence];
+  }
+
+  getAllOcurrences(){
+    return Array.from(this.Ocurrencelist);
+  }
+
+  addOcurrence(local, horario, data, descricao){
+    const ocurrence = new Ocurrence(local, horario, data, descricao)
+    this.ocurrence.add(ocurrence);
+  }
+
+  deleteOcurrence(ocurrence){
+    this.Ocurrencelist.delete(ocurrence);
+  }
+
+}
